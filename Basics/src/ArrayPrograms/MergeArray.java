9a06cb57375d7ad3ab4c0d85a16784c4fe497438
ELCOT <ELCOT@Lenovo>
2021-04-11 12:01:59 +0530

    mergeArray

---
+++ Basics/src/ArrayPrograms/MergeArray.java
@@ -0,0 +1,20 @@
+package ArrayPrograms;
+
+public class MergeArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x[] = { 7, 3, 4 };
+		int y[] = { 4, 5, 1, 9, 7 };
+		int z[]=new int[x.length+y.length];
+		for(int i=0;i<x.length;i++){
+			z[i]=x[i];
+			System.out.print(z[i]+" ");
+		}
+		for(int i=0;i<y.length;i++){
+			z[x.length+i]=y[i];
+			System.out.print(z[x.length+i]+" ");
+		}
+	}
+
+}
