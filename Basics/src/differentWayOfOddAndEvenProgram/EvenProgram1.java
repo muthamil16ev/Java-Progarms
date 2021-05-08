e4b9202bc821a0c584412ded5628fa4ea4843a48
ELCOT <ELCOT@Lenovo>
2021-05-08 13:01:19 +0530

    odd/even programs

---
+++ Basics/src/differentWayOfOddAndEvenProgram/EvenProgram1.java
@@ -0,0 +1,12 @@
+package differentWayOfOddAndEvenProgram;
+
+public class EvenProgram1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 2; i <= 100; i = i + 2) {
+			System.out.println(i);
+		}
+	}
+
+}
